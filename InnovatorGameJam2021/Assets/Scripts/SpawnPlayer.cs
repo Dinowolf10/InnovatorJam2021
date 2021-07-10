900dfb2de5d0bd1c5104f1795041cccd0e8ef260
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-07-09 22:58:31 -0400

    Created Level 1 layout and added spawn point

---
+++ InnovatorGameJam2021/Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.transform.position = spawnPoint.transform.position;
+    }
+}
