2dd308557d8aeb70156622bd9ab7932a92e8f427
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-07-10 00:06:10 -0400

    Added pool that "completes" level

---
+++ InnovatorGameJam2021/Assets/Scripts/CompleteLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteLevel : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
