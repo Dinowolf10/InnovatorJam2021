0788427796e0abd1e7812b0acf5eca608ed0bcf0
natedog334 <58520743+natedog334@users.noreply.github.com>
2021-07-10 22:17:02 -0400

    Added Level 4

---
+++ InnovatorGameJam2021/Assets/Scripts/DespawnEnemies.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnEnemies : MonoBehaviour
+{
+    public GameObject enemy1;
+
+    public GameObject enemy2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(enemy1.gameObject);
+            Destroy(enemy2.gameObject);
+        }
+    }
+}
